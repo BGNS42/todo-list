c78a34e6312c50ae7de80143ada2fa722f5f3f54
BGNS42 <196232533+BGNS42@users.noreply.github.com>
2026-04-14 11:21:00 -0300

    modificação do event listener de submit para verificar se está no modo add ou edit. Atualização do cancel e click no dialog para resetar estado

--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
 import "./style/styles.css";
 import "./style/pages.css";
 import { todoForm } from "./components/todoForm";
-import { createTodoFromForm } from "./app/controler";
+import { createTodoFromForm, updateTodoFromForm, resetFormState } from "./app/controler";
 
 // needed just for examples to show
 import { Todo } from "./components/todos";
 import { addProject } from "./app/state";
 import { showTodo } from "./components/todoCard";
 
-
 const dglog = todoForm();
-// document.body.appendChild(dglog); // todoForm() já controla isso
 
 const addBtn = document.querySelector(".btn-add");
-
 const form = dglog.querySelector("form");
 const cancelBtn = dglog.querySelector("#cancel");
 
-// Just a todo example to prevent the need to do the form all again
-const exampleTodo = new Todo("Example Todo", "The Todo details should goes here", "26/03/2026", false, "Notes should be here like this.");
+const exampleTodo = new Todo("Example Todo", "The Todo details should goes here", "20026-03-26", false, "Notes should be here like this.");
 addProject(exampleTodo);
 showTodo(exampleTodo);
 
-const exampleTodo2 = new Todo("Example Todo 2", "The Todo details should goes here", "26/03/2026", false, "Notes should be here like this.");
+const exampleTodo2 = new Todo("Example Todo 2", "The Todo details should goes here", "2026-03-12", false, "Notes should be here like this.");
 addProject(exampleTodo2);
 showTodo(exampleTodo2);
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-form.addEventListener("submit", () => {
-    createTodoFromForm(dglog);
+    if (dglog.dataset.editing) {
+        updateTodoFromForm(dglog);
+    } else {
+        const newTodo = createTodoFromForm(dglog);
+        if (newTodo) {
+            showTodo(newTodo);
+        }
+    }
+
     form.reset();
+    resetFormState(dglog);
 });
-
 
 addBtn.addEventListener("click", () => {
     dglog.showModal();
@@ -39,11 +44,15 @@ addBtn.addEventListener("click", () => {
 
 cancelBtn.addEventListener("click", (e) => {
     e.preventDefault();
+    form.reset();
+    resetFormState(dglog);
     dglog.close();
 });
 
 dglog.addEventListener("click", (event) => {
     if (event.target === dglog) {
+      form.reset();
+      resetFormState(dglog);
       dglog.close();
     }
 });
